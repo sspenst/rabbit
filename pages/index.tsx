2bcc61bb0e93213e8742c0ad1c925dca1b893772
Spencer Spenst <spencerspenst@gmail.com>
2023-06-15 23:34:15 -0700

    better routing from home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 import { redirectToAuthCodeFlow } from '../helpers/authCodeWithPkce';
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     <div className='flex text-center w-full justify-center font-medium h-screen items-center gap-6'>
       <h1 className='text-2xl border-r border-neutral-500 pr-6 py-2'>Rabbit</h1>
-      <button className='hover:underline' onClick={redirectToAuthCodeFlow}>Start</button>
+      <button className='hover:underline' onClick={() => {
+        if (localStorage.getItem('accessToken')) {
+          router.push('/app');
+        } else {
+          redirectToAuthCodeFlow();
+        }
+      }}>Start</button>
     </div>
   );
 }
