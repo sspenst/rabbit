8f545a09266fae8b1fbccbc075a866fd46dc804d
Spencer Spenst <spencerspenst@gmail.com>
2024-04-23 21:15:41 -0700

    semi-interactive help modal

--- components/helpModal.tsx
+++ components/helpModal.tsx
@@ -60,12 +60,12 @@ export default function HelpModal({ audioFeatures, isOpen, onClose, track }: Mod
                   </div>
                 }
                 <span>2. Discover related tracks</span>
-                <div className='bg-green-500 text-black p-3 rounded-full w-fit flex gap-2 font-medium'>
+                <button className='bg-green-500 disabled:bg-neutral-500 disabled:opacity-40 text-black p-3 rounded-full enabled:hover:bg-green-300 transition flex gap-2 font-medium w-fit'>
                   <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={2} stroke='currentColor' className='w-6 h-6'>
                     <path strokeLinecap='round' strokeLinejoin='round' d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z' />
                   </svg>
                   <span className='hidden md:block pr-1'>Discover</span>
-                </div>
+                </button>
                 <span>
                   {'3. Refine your search with '}
                   <a
@@ -106,11 +106,11 @@ export default function HelpModal({ audioFeatures, isOpen, onClose, track }: Mod
                   </div>
                 </div>
                 <span>4. Save tracks you enjoy!</span>
-                <div className='text-green-500'>
-                  <svg width='32' height='32' viewBox='0 0 150 150' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                <button className='text-green-500 hover:text-green-300 w-fit'>
+                  <svg width='24' height='24' viewBox='0 0 150 150' fill='none' xmlns='http://www.w3.org/2000/svg'>
                     <path d='M125.784 35.0369C113.039 22.2916 92.9859 21.3682 79.1227 32.8994C79.1062 32.9135 77.318 34.3807 75 34.3807C72.6234 34.3807 70.9266 32.9416 70.8609 32.8853C57.0141 21.3682 36.9609 22.2916 24.2156 35.0369C17.6695 41.583 14.0625 50.2877 14.0625 59.5478C14.0625 68.808 17.6695 77.5127 24.0914 83.9228L64.3078 131.006C66.9844 134.14 70.882 135.938 75 135.938C79.1203 135.938 83.0156 134.14 85.6922 131.009L125.782 84.0611C139.301 70.5447 139.301 48.5533 125.784 35.0369Z' fill='currentColor' />
                   </svg>
-                </div>
+                </button>
               </div>
               <button
                 className='inline-flex justify-center px-4 py-2 mt-2 text-sm font-medium border border-transparent rounded-md bg-neutral-200 dark:bg-neutral-800 hover:bg-neutral-400 hover:dark:bg-neutral-600 transition'
